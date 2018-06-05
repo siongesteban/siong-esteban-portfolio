8cf9d79b1d9544a3ed4e8d0c177b2a5e96c79baf
Siong Esteban <siong.esteban@gmail.com>
2018-06-06 02:26:41 +0800

    Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
-import logo from '../static/logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Index from '../pages/Index';
 import '../styles/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default () => (
+  <Router>
+    <Route path="/" component={Index} exact />
+  </Router>
+);

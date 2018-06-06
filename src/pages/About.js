410edfcd5b6a3bfc4383dbb261c47b1f80e84b8f
Siong Esteban <siong.esteban@gmail.com>
2018-06-06 16:46:31 +0800

    Add about page content.

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,13 @@ import Container from '../components/Container';
 export default () => (
   <Container>
     <p>
-      Hi! I'm <strong>Siong Esteban</strong>.
+      Hi, there. I'm <strong>Siong Esteban</strong>. I've been building full-stack web applications with different technologies throughout my college years. And guess what, I just graduated! Now I'm looking for a job as a developer where I can help build software solutions with the technology stack I'm comfortable with and further enhance my skills.
+    </p>
+    <p>
+      When I'm not coding, you can see me making myself busy with taking photographs. If it wasn't for Computer Science, maybe I chose photography as a career. I'm also into filmmaking, btw.
+    </p>
+    <p>
+      Previously, I had an internship where I worked as a web developer and built a full-stack web application to help manage, review and generate reports of health facility data.
     </p>
   </Container>
 )

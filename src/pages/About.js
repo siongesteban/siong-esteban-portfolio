1b24c0f12890ebddcbe76a6d22cfba6a6504f32a
Siong Esteban <siong.esteban@gmail.com>
2019-08-26 15:41:49 +0800

    Apply title in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,35 @@
 import React from 'react';
+
 import PageTitle from '../components/PageTitle';
 import Container from '../components/Container';
 
 export default () => (
   <div>
-    <PageTitle />
+    <PageTitle title="About" />
     <Container col={8}>
       <p>
-        Hi, there. I'm <strong>Siong Esteban</strong> (Jerico is my real name). I've been building full-stack web applications with different technologies throughout my college years. And guess what, I just graduated! Now I'm looking for a job as a developer where I can help build software solutions with the technology stack I'm comfortable with and further enhance my skills.
+        Hi, there. I&apos;m <strong>Siong Esteban</strong> (Jerico is my real
+        name). I&apos;ve been building full-stack web applications with
+        different technologies throughout my college years. And guess what, I
+        just graduated! Now I&apos;m looking for a job as a developer where I
+        can help build software solutions with the technology stack I&apos;m
+        comfortable with and further enhance my skills.
       </p>
       <p>
-        I'm currenly living in the Philippines but also willing to work overseas :).
+        I&apos;m currenly living in the Philippines but also willing to work
+        overseas :).
       </p>
       <p>
-        When I'm not coding, you can see me making myself busy with taking photographs. If it wasn't for Computer Science, maybe I chose photography as a career. I'm also into filmmaking, btw.
+        When I&apos;m not coding, you can see me making myself busy with taking
+        photographs. If it wasn&apos;t for Computer Science, maybe I chose
+        photography as a career. I&apos;m also into filmmaking, btw.
       </p>
       <p>
-        Previously, I had an internship where I worked as a web developer and built a full-stack web application to help the staffs to generate reports, manage and review data from different health facilities over the country.
+        Previously, I had an internship where I worked as a web developer and
+        built a full-stack web application to help the staffs to generate
+        reports, manage and review data from different health facilities over
+        the country.
       </p>
     </Container>
   </div>
-)
+);

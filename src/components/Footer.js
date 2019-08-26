178c731578f583128d39b019f5c4faa42b43e25d
Siong Esteban <siong.esteban@gmail.com>
2019-08-26 11:11:41 +0800

    Remove facebook from network icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import networkLinks from '../data/networkLinks';
+
 import '../styles/components/footer.css';
 
 export default () => (

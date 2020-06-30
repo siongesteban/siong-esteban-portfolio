25e072b4a8cc1958c309e968c5585fb1b1b94aea
Siong Esteban <siong.esteban@gmail.com>
2020-06-30 23:43:47 +0800

    Restructure skill set data

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,70 +1,30 @@
 export default [
   {
     role: 'Language',
-    groups: [
-      {
-        line: '1',
-        items: ['JavaScript/ES6', 'TypeScript', 'PHP'],
-      },
-    ],
+    groups: [['JavaScript/ES6', 'TypeScript', 'PHP']],
   },
   {
     role: 'Front End',
     groups: [
-      {
-        line: '1',
-        items: ['React', 'Redux Thunk', 'Redux Saga'],
-      },
-      {
-        line: '2',
-        items: ['Redux Form', 'Formik', 'Yup', 'Joi'],
-      },
-      {
-        line: '3',
-        items: ['Styled Components', 'Sass'],
-      },
-      {
-        line: '4',
-        items: ['Material UI', 'Semantic UI', 'Bootstrap'],
-      },
-      {
-        line: '5',
-        items: ['PWA', 'Webpack', 'Babel'],
-      },
+      ['React', 'Redux Thunk', 'Redux Saga'],
+      ['Redux Form', 'Formik', 'Yup', 'Joi'],
+      ['Styled Components', 'Sass'],
+      ['Material UI', 'Semantic UI', 'Bootstrap'],
+      ['PWA', 'Webpack', 'Babel'],
     ],
   },
   {
     role: 'Back End',
     groups: [
-      {
-        line: '1',
-        items: ['Node.js', 'Express', 'REST', 'GraphQL'],
-      },
-      {
-        line: '2',
-        items: ['MongoDB', 'PostgreSQL', 'MySQL'],
-      },
-      {
-        line: '3',
-        items: ['Firebase Authentication/Firestore'],
-      },
-      {
-        line: '4',
-        items: ['Mongoose', 'Sequelize', 'Prisma'],
-      },
-      {
-        line: '5',
-        items: ['CodeIgniter', 'Laravel'],
-      },
+      ['Node.js', 'Express', 'REST', 'GraphQL'],
+      ['MongoDB', 'PostgreSQL', 'MySQL'],
+      ['Firebase Authentication/Firestore'],
+      ['Mongoose', 'Sequelize', 'Prisma'],
+      ['CodeIgniter', 'Laravel'],
     ],
   },
   {
     role: 'Testing',
-    groups: [
-      {
-        line: '1',
-        items: ['Mocha', 'Chai', 'Jest', 'Enzyme'],
-      },
-    ],
+    groups: [['Mocha', 'Chai', 'Jest', 'Enzyme']],
   },
 ];
